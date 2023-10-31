9b4a30f222b90e6a2519cd255b1826f85c16b8dc
Mohamad Salimi <contact@salimi.my>
2023-10-31 21:02:39 +0800

    Update padding top

--- components/landing/experience.tsx
+++ components/landing/experience.tsx
@@ -93,7 +93,7 @@ export default function Experience() {
         What Skills I have
       </h1>
       <h2 className='text-center text-2xl pt-1'>Technical Level</h2>
-      <div className='grid lg:grid-cols-2 gap-8 pt-12'>
+      <div className='grid lg:grid-cols-2 gap-8 pt-8'>
         <div className='w-full rounded-2xl flex flex-col items-center bg-primary group hover:bg-primary-foreground border border-primary py-9 px-6 md:py-9 md:px-20'>
           <h3 className='text-lg md:text-xl text-primary-foreground group-hover:text-primary pb-8'>
             Frontend Development
