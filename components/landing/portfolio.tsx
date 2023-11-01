b1d3d72db179280e6eddf8f64393bd0e96ea1fbe
Mohamad Salimi <contact@salimi.my>
2023-11-01 21:48:44 +0800

    Rearrange import

--- components/landing/portfolio.tsx
+++ components/landing/portfolio.tsx
@@ -1,8 +1,8 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 
 const data_portfolio = [
   {
