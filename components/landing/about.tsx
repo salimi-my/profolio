aedb796677a45316a3b6954139a278a87ed2ede5
Mohamad Salimi <contact@salimi.my>
2023-11-01 21:50:04 +0800

    Rearrange import

--- components/landing/about.tsx
+++ components/landing/about.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Briefcase, FolderGit2, Laptop } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
