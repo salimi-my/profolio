751a31e1b28faa56723644156f526acdf69d181c
Mohamad Salimi <contact@salimi.my>
2023-11-02 14:10:40 +0800

    Update padding

--- components/landing/tool.tsx
+++ components/landing/tool.tsx
@@ -82,7 +82,7 @@ const data_tools = [
 
 export default function Tool() {
   return (
-    <section id='tool' className='my-32'>
+    <section id='tool' className='mt-32'>
       <h1 className='text-center text-sm text-muted-foreground font-medium'>
         What I Use
       </h1>
