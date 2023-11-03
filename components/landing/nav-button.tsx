e594e5bda295671383878bb43b17da986fce12e4
Mohamad Salimi <contact@salimi.my>
2023-11-03 18:58:29 +0800

    Add tooltip to nav

---
+++ components/landing/nav-button.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import { LucideIcon } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
+
+interface NavButtonProps {
+  name: string;
+  anchor: string;
+  icon: LucideIcon;
+  hideMobile?: boolean;
+}
+
+export default function NavButton({
+  name,
+  anchor,
+  icon: Icon,
+  hideMobile
+}: NavButtonProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <Button
+            variant='ghost'
+            size='sm'
+            className={cn(
+              'rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11',
+              hideMobile && 'hidden md:flex'
+            )}
+            asChild
+          >
+            <Link href={anchor}>
+              <Icon
+                className={cn(name === 'Qualification' ? 'w-6 h-6' : 'w-5 h-5')}
+              />
+            </Link>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent
+          sideOffset={16}
+          className='rounded-full bg-black bg-opacity-70 backdrop-blur-lg text-white'
+        >
+          <p className='font-medium'>{name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
--- components/landing/nav.tsx
+++ components/landing/nav.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import {
   Book,
   ClipboardCheck,
@@ -10,71 +9,26 @@ import {
   User
 } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
+import NavButton from '@/components/landing/nav-button';
 
 export default function Nav() {
   return (
     <nav className='bg-black bg-opacity-30 w-max py-3 px-7 fixed bottom-8 z-30 mx-auto inset-x-0 flex gap-3 rounded-full backdrop-blur-lg'>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11'
-        asChild
-      >
-        <Link href='#home'>
-          <Home className='w-5 h-5' />
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11'
-        asChild
-      >
-        <Link href='#about'>
-          <User className='w-5 h-5' />
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11'
-        asChild
-      >
-        <Link href='#experience'>
-          <Book className='w-5 h-5' />
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11 hidden md:flex'
-        asChild
-      >
-        <Link href='#expertise'>
-          <ClipboardCheck className='w-5 h-5' />
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11'
-        asChild
-      >
-        <Link href='#qualification'>
-          <GraduationCap className='w-6 h-6' />
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        className='rounded-full text-white px-0 w-8 h-8 xs:w-11 xs:h-11'
-        asChild
-      >
-        <Link href='#contact'>
-          <MessageCircle className='w-5 h-5' />
-        </Link>
-      </Button>
+      <NavButton name='Home' anchor='#home' icon={Home} />
+      <NavButton name='About' anchor='#about' icon={User} />
+      <NavButton name='Experience' anchor='#experience' icon={Book} />
+      <NavButton
+        name='Expertise'
+        anchor='#expertise'
+        icon={ClipboardCheck}
+        hideMobile
+      />
+      <NavButton
+        name='Qualification'
+        anchor='#qualification'
+        icon={GraduationCap}
+      />
+      <NavButton name='Contact' anchor='#contact' icon={MessageCircle} />
     </nav>
   );
 }
