ef42c87ffb4f8ee8f6379595fff5519cdf6c32a2
Mohamad Salimi <contact@salimi.my>
2023-11-04 20:51:07 +0800

    Create sidebar

--- app/(secured)/admin/(routes)/layout.tsx
+++ app/(secured)/admin/(routes)/layout.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/lib/utils';
 import useStore from '@/hooks/use-store';
 import Footer from '@/components/secured/footer';
+import Sidebar from '@/components/secured/sidebar';
 import useSidebarToggle from '@/hooks/use-sidebar-toggle';
 
 export default function AdminRouteLayout({
@@ -14,6 +15,7 @@ export default function AdminRouteLayout({
 
   return (
     <>
+      <Sidebar />
       <main
         className={cn(
           'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-800 transition-[margin-left] ease-in-out duration-300',
