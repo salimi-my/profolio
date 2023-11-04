1f0c437b05f65e2ec78aef1f0539ff0d221d3c95
Mohamad Salimi <contact@salimi.my>
2023-11-05 00:20:12 +0800

    Remove button

--- app/(secured)/admin/(routes)/page.tsx
+++ app/(secured)/admin/(routes)/page.tsx
@@ -1,12 +1,3 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
-import { signOut } from 'next-auth/react';
-
 export default function DashboardPage() {
-  return (
-    <div>
-      <Button onClick={() => signOut()}>Sign out</Button>
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
