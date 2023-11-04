4b399da6c4b70f69bf14a90ad6b37de639721aea
Mohamad Salimi <contact@salimi.my>
2023-11-05 00:03:49 +0800

    Update dark bg color

--- app/(secured)/admin/(routes)/layout.tsx
+++ app/(secured)/admin/(routes)/layout.tsx
@@ -19,7 +19,7 @@ export default function AdminRouteLayout({
       <Sidebar />
       <main
         className={cn(
-          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-800 transition-[margin-left] ease-in-out duration-300',
+          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
           sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
         )}
       >
