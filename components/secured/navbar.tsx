746d64fc5aa439ccbc5ad7b5d6e9664076f5207f
Mohamad Salimi <contact@salimi.my>
2023-11-05 23:29:23 +0800

    Add mobile sidebar menu

--- components/secured/navbar.tsx
+++ components/secured/navbar.tsx
@@ -1,12 +1,14 @@
 import UserNav from '@/components/secured/user-nav';
 import { ModeToggle } from '@/components/mode-toggle';
 import PageTitle from '@/components/secured/page-title';
+import SheetMenu from '@/components/secured/sheet-menu';
 
 export default function Navbar() {
   return (
     <header className='z-10 supports-backdrop-blur:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur'>
-      <div className='mx-8 flex h-14 items-center'>
+      <div className='mx-4 sm:mx-8 flex h-14 items-center'>
         <div className='flex items-center space-x-4 lg:space-x-0'>
+          <SheetMenu />
           <PageTitle />
         </div>
         <div className='flex flex-1 items-center space-x-2 justify-end'>
