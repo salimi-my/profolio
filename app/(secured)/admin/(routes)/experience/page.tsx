3f74498bed65a50ffb140cf213d514292fa09650
Mohamad Salimi <contact@salimi.my>
2023-11-05 14:44:41 +0800

    Create experience page

---
+++ app/(secured)/admin/(routes)/experience/page.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+export default async function ExperiencePage() {
+  return (
+    <div className='grid lg:grid-cols-2 gap-4'>
+      <Card className='rounded-lg border-none'>
+        <CardHeader className='mx-[1px] pb-9'>
+          <CardTitle className='text-xl font-bold'>
+            Frontend Development
+          </CardTitle>
+          <CardDescription>
+            Manage your frontend development section informations.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>FrontendDevelopment</CardContent>
+      </Card>
+      <Card className='rounded-lg border-none'>
+        <CardHeader className='mx-[1px] pb-9'>
+          <CardTitle className='text-xl font-bold'>
+            Backend Development
+          </CardTitle>
+          <CardDescription>
+            Manage your backend development section informations.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>BackendDevelopment</CardContent>
+      </Card>
+    </div>
+  );
+}
