79fe062fa2c49683e77d5f8f3ef1ee57b88269ba
Mohamad Salimi <contact@salimi.my>
2023-11-06 20:07:00 +0800

    Update mode toggle bg color

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -28,7 +28,7 @@ export function ModeToggle() {
           <TooltipTrigger asChild>
             <DropdownMenuTrigger asChild>
               <Button
-                className='rounded-full w-8 h-8'
+                className='rounded-full w-8 h-8 bg-background'
                 variant='outline'
                 size='icon'
               >
