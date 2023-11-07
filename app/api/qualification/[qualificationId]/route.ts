e570733fe8fe5b377f2afe312e5f2f9b39b1f4db
Mohamad Salimi <contact@salimi.my>
2023-11-08 01:12:19 +0800

    Update null value

--- app/api/qualification/[qualificationId]/route.ts
+++ app/api/qualification/[qualificationId]/route.ts
@@ -95,10 +95,10 @@ export async function PATCH(
       },
       data: {
         type,
-        degree,
-        school,
-        position,
-        company,
+        degree: degree === '' ? null : degree,
+        school: school === '' ? null : school,
+        position: position === '' ? null : position,
+        company: company === '' ? null : company,
         startYear,
         endYear
       }
