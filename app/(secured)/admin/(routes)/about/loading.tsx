396677cb2a31a56eb12f94941014082020f971ee
Mohamad Salimi <contact@salimi.my>
2023-11-12 16:28:16 +0800

    Add skeleton loading for about page

---
+++ app/(secured)/admin/(routes)/about/loading.tsx
@@ -0,0 +1,46 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+export default function AboutLoading() {
+  return (
+    <Card className='rounded-lg border-none'>
+      <CardHeader className='mx-[1px] pb-9'>
+        <CardTitle className='text-xl font-semibold'>
+          <Skeleton className='h-7 w-[100px]' />
+        </CardTitle>
+        <CardDescription>
+          <Skeleton className='h-5 w-[300px]' />
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className='flex flex-col gap-4'>
+          <div className='space-y-2'>
+            <Skeleton className='h-5 w-[150px]' />
+            <Skeleton className='h-9 w-full' />
+          </div>
+          <div className='space-y-2'>
+            <Skeleton className='h-5 w-[150px]' />
+            <Skeleton className='h-9 w-full' />
+          </div>
+          <div className='space-y-2'>
+            <Skeleton className='h-5 w-[150px]' />
+            <Skeleton className='h-9 w-full' />
+          </div>
+          <div className='space-y-2'>
+            <Skeleton className='h-5 w-[150px]' />
+            <Skeleton className='h-[118px] w-full' />
+          </div>
+          <div>
+            <Skeleton className='h-9 w-24 mt-2' />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
