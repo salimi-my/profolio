ab98bbed9d4d731f2b7de902fbd71d41809d4e22
Mohamad Salimi <contact@salimi.my>
2023-11-12 11:50:42 +0800

    Update qualification timeline color

--- components/secured/qualification-timeline.tsx
+++ components/secured/qualification-timeline.tsx
@@ -25,7 +25,7 @@ export default function QualificationTimeline({
             )}
           >
             <div className='order-1 w-5/12'></div>
-            <div className='z-20 flex items-center order-1 bg-primary shadow-xl w-6 h-6 rounded-full border-4 border-primary-foreground' />
+            <div className='z-20 flex items-center order-1 bg-zinc-400 w-6 h-6 rounded-full border-4 border-primary-foreground' />
             <div className='order-1 w-5/12 text-primary'>
               <div
                 className={cn(
@@ -33,16 +33,16 @@ export default function QualificationTimeline({
                   index % 2 === 0 && 'flex-row-reverse'
                 )}
               >
-                <div className='rounded-full bg-primary-foreground w-6 h-6 flex justify-center items-center p-1'>
+                <div className='rounded-full bg-muted w-6 h-6 flex justify-center items-center p-1'>
                   <Calendar className='w-4 h-4' />
                 </div>
-                <div className='rounded-lg bg-primary-foreground px-2 py-1'>
+                <div className='rounded-lg bg-muted px-2 py-1'>
                   <p className='text-xs font-medium'>
                     {value.startYear} - {value.endYear}
                   </p>
                 </div>
               </div>
-              <div className='rounded-lg bg-primary-foreground py-2 px-3 mt-3'>
+              <div className='rounded-lg bg-muted py-2 px-3 mt-3'>
                 <h4 className='font-medium leading-5'>
                   {qualificationType === 'education'
                     ? value.degree
