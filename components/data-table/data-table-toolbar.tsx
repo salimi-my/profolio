0a80ddebb7e41cc76a32fc476ee523c4f9b531cf
Mohamad Salimi <contact@salimi.my>
2023-11-12 23:25:44 +0800

    Add input name

--- components/data-table/data-table-toolbar.tsx
+++ components/data-table/data-table-toolbar.tsx
@@ -36,6 +36,7 @@ export function DataTableToolbar<TData>({
           <MagnifyingGlassIcon className='h-4 w-4 mx-2 text-muted-foreground' />
           <Input
             placeholder='Search...'
+            name='search'
             value={(table.getState().globalFilter as string) ?? ''}
             onChange={(event) => table.setGlobalFilter(event.target.value)}
             className='h-8 w-[120px] lg:w-[250px] border-none shadow-none pl-0 focus-visible:ring-0'
