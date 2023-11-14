f258d0a0ecabefe7dfb20155b4a90d4595eab98a
Mohamad Salimi <contact@salimi.my>
2023-11-14 18:02:02 +0800

    Add handler type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 
 import { config } from '@/lib/auth';
 
-export const handler = NextAuth(config);
+export const handler: NextAuthOptions = NextAuth(config);
 
 export { handler as GET, handler as POST };
