54930efbffa4f32e524b76e26784325322a0cb75
Mohamad Salimi <contact@salimi.my>
2023-11-14 18:16:15 +0800

    Remove export handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
 
 import { config } from '@/lib/auth';
 
-export const handler: NextAuthOptions = NextAuth(config);
+const handler = NextAuth(config);
 
 export { handler as GET, handler as POST };
