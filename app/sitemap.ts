2f55208f5cd114c58e45eb677765425ff3b38ab9
Mohamad Salimi <contact@salimi.my>
2023-11-20 00:14:37 +0800

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,28 @@
 import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+import prismadb from '@/lib/prismadb';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.APP_URL
     ? `${process.env.APP_URL}`
     : process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : `http://localhost:${process.env.PORT || 3000}`;
 
-  return [
+  const portfolios = await prismadb.portfolio.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  const portfoliosMap: MetadataRoute.Sitemap = portfolios.map((portfolio) => ({
+    url: portfolio.demoUrl,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5
+  }));
+
+  const defaultMap: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -27,4 +42,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8
     }
   ];
+
+  return [...defaultMap, ...portfoliosMap];
 }
