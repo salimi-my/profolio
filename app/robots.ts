f58fab153e583ad7c354eb8a3a7db16fdf1b8d1e
Mohamad Salimi <contact@salimi.my>
2023-11-19 23:47:13 +0800

    Add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.APP_URL
+    ? `${process.env.APP_URL}`
+    : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT || 3000}`;
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/admin/'
+    },
+    sitemap: `${baseUrl}/sitemap.xml`
+  };
+}
