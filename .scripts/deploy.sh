aac2be0925b02742448550ad703281cd294eab57
Mohamad Salimi <contact@salimi.my>
2023-11-25 18:26:23 +0800

    Add workflows

---
+++ .scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server!"
+
+echo "Installing Dependencies..."
+pnpm install
+
+echo "Creating Production Build..."
+pnpm run build
+
+echo "PM2 Reload"
+pm2 restart www.salimi.my
+
+echo "Deployment Finished!"
