8a0ad664bc0b8a1b6ef386497678b378f87f77e8
Mohamad Salimi <contact@salimi.my>
2024-01-31 02:11:13 +0800

    Add edgestore api into public routes

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes does not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/'];
+export const publicRoutes = ['/', '/api/edgestore/init'];
 
 /**
  * An array of routes that are used for authentication
