1ab7ca03ece43c9e2e9079525dbaf054ee638778
Mohamad Salimi <contact@salimi.my>
2024-02-06 01:47:08 +0800

    Update query

--- data/information.ts
+++ data/information.ts
@@ -36,81 +36,92 @@ interface Data {
 }
 
 export default async function getInformation(): Promise<Data> {
+  const aboutPromise = prismadb.about.findFirst();
+
+  const experiencesPromise = prismadb.experience.findMany({
+    where: {
+      type: { in: ['FRONTEND', 'BACKEND'] }
+    }
+  });
+
+  const expertisesPromise = prismadb.expertise.findMany({
+    where: {
+      type: { in: ['SEOOPTIMIZATION', 'WEBDEVELOPMENT', 'CONTENTCREATION'] }
+    }
+  });
+
+  const qualificationsPromise = prismadb.qualification.findMany({
+    where: {
+      type: { in: ['EDUCATION', 'EXPERIENCE'] }
+    },
+    orderBy: {
+      id: 'desc'
+    }
+  });
+
+  const portfolioPromise = prismadb.portfolio.findMany({
+    take: 6,
+    include: {
+      tags: true
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  const miscellaneousPromise = prismadb.miscellaneous.findFirst({
+    include: {
+      titles: true
+    }
+  });
+
+  const toolPromise = prismadb.tool.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
   const [
     about,
-    frontend,
-    backend,
-    seooptimization,
-    webdevelopment,
-    contentcreation,
-    education,
-    experience,
+    experiences,
+    expertises,
+    qualifications,
     portfolio,
     miscellaneous,
     tool
-  ] = await prismadb.$transaction([
-    prismadb.about.findFirst(),
-    prismadb.experience.findMany({
-      where: {
-        type: 'FRONTEND'
-      }
-    }),
-    prismadb.experience.findMany({
-      where: {
-        type: 'BACKEND'
-      }
-    }),
-    prismadb.expertise.findMany({
-      where: {
-        type: 'SEOOPTIMIZATION'
-      }
-    }),
-    prismadb.expertise.findMany({
-      where: {
-        type: 'WEBDEVELOPMENT'
-      }
-    }),
-    prismadb.expertise.findMany({
-      where: {
-        type: 'CONTENTCREATION'
-      }
-    }),
-    prismadb.qualification.findMany({
-      where: {
-        type: 'EDUCATION'
-      },
-      orderBy: {
-        id: 'desc'
-      }
-    }),
-    prismadb.qualification.findMany({
-      where: {
-        type: 'EXPERIENCE'
-      },
-      orderBy: {
-        id: 'desc'
-      }
-    }),
-    prismadb.portfolio.findMany({
-      take: 6,
-      include: {
-        tags: true
-      },
-      orderBy: {
-        createdAt: 'desc'
-      }
-    }),
-    prismadb.miscellaneous.findFirst({
-      include: {
-        titles: true
-      }
-    }),
-    prismadb.tool.findMany({
-      orderBy: {
-        createdAt: 'desc'
-      }
-    })
+  ] = await Promise.all([
+    aboutPromise,
+    experiencesPromise,
+    expertisesPromise,
+    qualificationsPromise,
+    portfolioPromise,
+    miscellaneousPromise,
+    toolPromise
   ]);
+
+  const frontend = experiences.filter(
+    (experience) => experience.type === 'FRONTEND'
+  );
+  const backend = experiences.filter(
+    (experience) => experience.type === 'BACKEND'
+  );
+
+  const seooptimization = expertises.filter(
+    (expertise) => expertise.type === 'SEOOPTIMIZATION'
+  );
+  const webdevelopment = expertises.filter(
+    (expertise) => expertise.type === 'WEBDEVELOPMENT'
+  );
+  const contentcreation = expertises.filter(
+    (expertise) => expertise.type === 'CONTENTCREATION'
+  );
+
+  const education = qualifications.filter(
+    (qualification) => qualification.type === 'EDUCATION'
+  );
+  const experience = qualifications.filter(
+    (qualification) => qualification.type === 'EXPERIENCE'
+  );
 
   const portfolioWithBlur = await addBlurredDataUrls(portfolio);
 
