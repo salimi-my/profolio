2f4120b04bd615b84db4258158920c38b5fd48c4
Mohamad Salimi <contact@salimi.my>
2024-02-09 00:03:07 +0800

    Update comments

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes does not require authentication
  * @type {string[]}
  */
-export const publicRoutes = [
+export const publicRoutes: string[] = [
   '/',
   '/privacy-policy',
   '/terms-and-conditions',
@@ -16,20 +16,20 @@ export const publicRoutes = [
 
 /**
  * An array of routes that are used for authentication
- * These routes will redirect signed in users to /settings
+ * These routes will redirect signed in users to /admin
  * @type {string[]}
  */
-export const authRoutes = ['/auth/sign-in', '/auth/sign-up'];
+export const authRoutes: string[] = ['/auth/sign-in', '/auth/sign-up'];
 
 /**
  * The prefix for API authentication routes
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = '/api/auth';
+export const apiAuthPrefix: string = '/api/auth';
 
 /**
- * The default redirect path after logging in
+ * The default redirect path after signing in
  * @type {string}
  */
-export const DEFAULT_SIGNIN_REDIRECT = '/admin';
+export const DEFAULT_SIGNIN_REDIRECT: string = '/admin';
