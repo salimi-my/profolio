80cab5d31bd8cf62f4d2dad3f4fe7c7e5bc119d0
Mohamad Salimi <contact@salimi.my>
2024-05-23 23:56:46 +0800

    style: fix tailwind support rule

--- components/admin-panel/footer.tsx
+++ components/admin-panel/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className='supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur'>
+    <div className='supports-[backdrop-filter]:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur'>
       <div className='mx-8 flex h-14 items-center'>
         <small className='text-muted-foreground'>
           Created by{' '}
--- components/admin-panel/navbar.tsx
+++ components/admin-panel/navbar.tsx
@@ -9,7 +9,7 @@ interface NavbarProps {
 
 export function Navbar({ title }: NavbarProps) {
   return (
-    <header className='z-10 supports-backdrop-blur:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur'>
+    <header className='z-10 supports-[backdrop-filter]:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur'>
       <div className='mx-4 sm:mx-8 flex h-14 items-center'>
         <div className='flex items-center space-x-4 lg:space-x-0'>
           <SheetMenu />
